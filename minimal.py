78a810500aac641a8bdfe7b193ce9dd7a106e132
Moshe Zadka <zadka.moshe@gmail.com>
2020-10-30 18:17:05 -0700

    frop

--- minimal.py
+++ minimal.py
@@ -1,22 +1,52 @@
-from prometheus_client import CollectorRegistry, Gauge
+from prometheus_client import (
+    CollectorRegistry,
+    Gauge, Counter,
+)
 from pyramid.config import Configurator
 from pyramid.response import Response
-from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+from prometheus_client import (
+    generate_latest,
+    CONTENT_TYPE_LATEST,
+)
 import random
 
-def registry_from_num(num):
-    registry = CollectorRegistry()
-    g = Gauge("level", "The level", registry=registry)
-    g.set(num)
-    return registry
+
+def update(request):
+    num = random.uniform(0, 1)
+    request.registry["hits"].inc()
+    request.registry["level"].set(num)
+
 
 def metrics_web(request):
-    num = random.uniform(0, 1)
-    registry = registry_from_num(num)
-    return Response(generate_latest(registry),
-                    content_type=CONTENT_TYPE_LATEST)
+    update(request)
+    registry = request.registry[
+        "prometheus_registry"
+    ]
+    return Response(
+        generate_latest(registry),
+        content_type=CONTENT_TYPE_LATEST,
+    )
+
+
+def configure_metrics(mapping):
+    registry = CollectorRegistry()
+    mapping["prometheus_registry"] = registry
+    mapping["level"] = Gauge(
+        "level",
+        "The level",
+        registry=registry,
+    )
+    mapping["hits"] = Counter(
+        "hits",
+        "Hits to endpoint",
+        registry=registry,
+    )
+
 
 with Configurator() as config:
-    config.add_route('metrics', '/metrics')
-    config.add_view(metrics_web, route_name='metrics')
+    configure_metrics(config.registry)
+    config.add_route("metrics", "/metrics")
+    config.add_view(
+        metrics_web, route_name="metrics"
+    )
     application = config.make_wsgi_app()
